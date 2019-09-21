3604170aa168168319f91c84d5588e94e65db79e
Gee-Wey Yue <geewey@gmail.com>
2019-09-21 06:39:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,41 +44,23 @@ def reduce_to_total(source_array, starting_point=0)
   return sum
 end
 
-  describe "reduce_to_all_true returns true when all values are truthy" do
-    it "reduces correctly" do
-      source_array = [1, 2, true, "razmatazz"]
-      expect(reduce_to_all_true(source_array)).to be_truthy
-    end
-  end
-
-  describe "reduce_to_all_true returns false when any value is false" do
-    it "reduces correctly" do
-      source_array = [1, 2, true, "razmatazz", false]
-      expect(reduce_to_all_true(source_array)).to be_falsy
-    end
-  end
   
 def reduce_to_all_true(source_array)
-  
-end
-
-
-  describe "reduce_to_any_true returns true when a truthy value is present" do
-    it "reduces correctly" do
-      source_array = [ false, nil, nil, nil, true]
-      expect(reduce_to_any_true(source_array)).to eq(true)
+  source_array.each do |ele|
+    unless ele
+      return false
     end
   end
-
-  describe "reduce_to_any_true returns false when no truthy value is present" do
-    it "reduces correctly" do
-      source_array = [ false, nil, nil, nil]
-      expect(reduce_to_any_true(source_array)).to eq(false)
-    end
-  end
+  return true
 end
+
 
 def reduce_to_any_true(source_array)
-  
+  source_array.each do |ele|
+    if ele
+      return true
+    end
+  end
+  return false
 end
 
